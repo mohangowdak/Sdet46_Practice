9f50cf8383efd7d4295b1469cd2115d8e025c99c
mohangowdak <mohan.k@testyantra.com>
2023-03-10 16:19:38 +0530

    b

--- src/test/java/practice/PracticeClass1.java
+++ src/test/java/practice/PracticeClass1.java
@@ -6,12 +6,16 @@ public class PracticeClass1 {
 	
 	@Test
 	public void main1() {
+		String url = System.getProperty("url");
 		System.out.println("Test1 --> abcd");
+		System.out.println("URL of Application ==> "+url);
 	}
 	
 	@Test
 	public void main11() {
+		String browser = System.getProperty("browser");
 		System.out.println("Test1 --> xyz");
+		System.out.println("Browser Name ==> "+browser);
 	}
 
 }
